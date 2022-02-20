e8dd04dccbaafe57fc8e5c0fbbf36d4a4be112d5
sanjay <sanjay.sanjaykandpal4@gmail.com>
2022-02-20 20:25:35 +0530

    ejs installed

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 8000;
 
 // use express router
 app.use('/',require('./routes'));
+// set up the view engine ejs
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
