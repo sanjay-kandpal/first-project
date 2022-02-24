0dc8274e56a1028799930d958918544172f57c90
sanjay <sanjay.sanjaykandpal4@gmail.com>
2022-02-24 21:15:42 +0530

    add scss to project

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
-
+const 
 
 app.use(express.urlencoded());
 
