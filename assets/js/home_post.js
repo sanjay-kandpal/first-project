93a5b9195186519c39fc5e7f3943c7e4e6681429
sanjay <sanjay.sanjaykandpal4@gmail.com>
2022-03-08 17:15:01 +0530

    Creating a post - Recieving data

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -11,6 +11,8 @@ let createPost = function(){
             url: '/posts/create',
             data: newPostForm.serialize(),
             success: function(data){
+                let newPost = newPostDOM(data.data.post);
+                $('#posts-list-container>ul').prepend.(newPost); 
                 console.log(data);
             },error: function(err){
                 console.log(error.responseText);
@@ -18,6 +20,39 @@ let createPost = function(){
         })
     });
 }
+ // method to create a post in DOM
+ let newPostDOM = function(post){
+     return $(` <li id="post-${post._id}"> 
+     <p>
+        
+            <a class="delete-post-button" href="/posts/destroy/ ${post.id }" type="submit">Delete Post</a>
+            <%}%>
+        ${ post.content }
+        <br>
+        <small>
+             ${post.user.name}
+        </small>
+     </p>
+     <div class="post-comments">
+       
+            <form action="/comments/create" method="POST">
+                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                <input type="hidden" name="post" value="${ post._id}" >
+                <input type="submit" value="Add Comment">
+                
+            </form>
+    
+      
+    
+        <div class="post-comments-list">
+            <ul id="post-comments-${post._id}">
+                
+            </ul>
+        </div>
+     </div>
+    
+    </li> `)
+ }
 createPost();
 
 }
