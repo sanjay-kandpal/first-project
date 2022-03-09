f7cbc76132ed5380c46b59592eccef58854720a2
sanjay <sanjay.sanjaykandpal4@gmail.com>
2022-03-09 21:07:24 +0530

    Showing the avatars ,connecting the dots

--- index.js
+++ index.js
@@ -50,9 +50,12 @@ app.use(sassMiddleware({
 }))
 app.use(express.urlencoded());
 
+
 app.use(cookieParser());
 
 app.use(express.static('./assets'));
+// make uploads path available to the browser
+app.use('/uploads',express.static(__dirname + '/uploads'));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
