17c75ec9f794c38855bcc6f4e4e197c1de5fceba
sanjay <sanjay.sanjaykandpal4@gmail.com>
2022-03-12 18:15:53 +0530

    playing with api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,47 @@
-module.exports.index = function(req,res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment')
+module.exports.index = async function(req,res){
+ 
+    
+        let posts = await Post.find({})
+        .sort('-createdAt')
+       .populate('user')
+       .populate({
+         path: 'comment',
+         populate: {
+           path: 'user'
+         }
+       });
+
     return res.json(200,{
         message: "List of Posts",
-        posts: []
+        posts: posts
     })
 }
+module.exports.destroy = async function(req,res){
+    try
+    { 
+   let post = await Post.findById(req.params.id);
+  
+   //   if(post.user == req.user.id){
+          post.remove();
+       await  Comment.deleteMany({post: req.params.id});
+      
+        
+           
+      
+       return res.json(200,{
+           message: 'Post and associated message deleted successfully'
+       });
+    //  }else{
+    //      req.flash('success','You cannot delete this post ');
+    //      return res.redirect('back');
+    //  }
+   }
+   catch(err){
+       return res.json(500,{
+           message: 'internal server error'
+       })   
+      
+   }    
+  }
