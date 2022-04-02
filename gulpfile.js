16efb2259481def497f81897fd8b7c918ff84f3f
sanjay <sanjay.sanjaykandpal4@gmail.com>
2022-04-02 08:59:52 +0530

    Completing gulp- css and js minification not done throwing error

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssnano = require('gulp-cssnano');
 const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const del =require('del');
 
-gulp.task('css',function(){
+
+gulp.task('css',function(done){
     console.log('minifying css...');
     gulp.src('./assets/sass/**/*.scss')
     .pipe(sass())
@@ -19,4 +22,45 @@ gulp.task('css',function(){
         merge: true
     }))
     .pipe(gulp.dest('./public/assets'));
+    done();
+});
+// Throwing err
+// gulp.task('js',function(done){
+//     console.log('minifying js...');
+//     gulp.src('./assets/**/*.js')
+//     .pipe(uglify())
+//     .pipe(rev())
+//     .pipe(gulp.dest('./public/assets'))
+//     .pipe(rev.manifest({
+//         cwd: 'public',
+//         merge: true
+//     }))
+//     .pipe(gulp.dest('./public/assets'));
+//     done();
+// });
+//  imagemin ES6 module not working in ES5
+// gulp.task('images',function(done){
+//     console.log('compressing images...');
+//     gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg')
+//     .pipe(imagemin())
+//     .pipe(rev())
+//     .pipe(gulp.dest('./assets/public'))
+//     .pipe(rev.manifest({
+//         cwd: 'public',
+//         merge: true
+//     }))
+//     .pipe(gulp.dest('./public/assets'));
+//     done();
+// });
+
+
+//empty the public/assets directory
+gulp.task('clean:assets',function(done){
+    del.sync('./public/assets');
+    done();
+});
+
+gulp.task('build',gulp.series('clean:assets','css'),function(done){
+    console.log('Building assets');
+    done();
 })
